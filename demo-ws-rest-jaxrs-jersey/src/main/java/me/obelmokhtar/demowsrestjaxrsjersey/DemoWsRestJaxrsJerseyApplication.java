e0e5a6a9a7e8b13cb2f47e8b01849d53d3689ad8
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2022-03-24 13:14:42 +0100

    just creating the project

---
+++ demo-ws-rest-jaxrs-jersey/src/main/java/me/obelmokhtar/demowsrestjaxrsjersey/DemoWsRestJaxrsJerseyApplication.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.demowsrestjaxrsjersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWsRestJaxrsJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWsRestJaxrsJerseyApplication.class, args);
+	}
+
+}
---
+++ demo-ws-rest-jaxrs-jersey/src/test/java/me/obelmokhtar/demowsrestjaxrsjersey/DemoWsRestJaxrsJerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.demowsrestjaxrsjersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWsRestJaxrsJerseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
